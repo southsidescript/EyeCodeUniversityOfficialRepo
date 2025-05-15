f8c50150fbf502b1e9826c5d523497cffaec56ef
Serkhan <106021920+southsidescript@users.noreply.github.com>
2025-05-15 10:04:41 +0300

    gameBetaVersion

--- src/Components/sections/Event/SummerEvent.jsx
+++ src/Components/sections/Event/SummerEvent.jsx
@@ -1,5 +1,5 @@
 import styles from "./styles.module.css"
-import { CodeIcon, Calendar, Users, Gift, Trophy, MessageCircle, Star, GitFork } from "lucide-react"
+import { CodeIcon, Calendar, Users, Gift, Trophy,  Star, GitFork, } from "lucide-react"
 
 const EyeCodeBattlePage = () => {
   return (
@@ -43,7 +43,7 @@ const EyeCodeBattlePage = () => {
           <section className={styles.section}>
             <div className={styles.hero}>
               <h1 className={styles.heroTitle}>EyeCode Battle: Summer Edition</h1>
-              <p className={styles.heroSubtitle}>Регистрация открыта! 🎉</p>
+              <p className={styles.heroSubtitle}>Регистрация закрыта!</p>
 
               <div className={styles.eventDetails}>
                 <div className={styles.eventDetail}>
@@ -56,7 +56,7 @@ const EyeCodeBattlePage = () => {
                 </div>
               </div>
 
-              <a href="https://wa.me/79283526584?text=%D0%94%D0%BE%D0%B1%D1%80%D1%8B%D0%B9%20%D0%B2%D0%B5%D1%87%D0%B5%D1%80%21%0A%D0%A5%D0%BE%D1%87%D1%83%20%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D0%B0%D1%82%D1%8C%20%D1%81%D0%B2%D0%BE%D1%8E%20%D0%BA%D0%BE%D0%BC%D0%B0%D0%BD%D0%B4%D1%83%20%D0%BD%D0%B0%20%D1%81%D0%BE%D1%80%D0%B5%D0%B2%D0%BD%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5%20EyeCode%20Battle%20%3A%20Summer%20edition%20!" className={styles.button}>
+              <a  href="https://wa.me/79283526584?text=%D0%94%D0%BE%D0%B1%D1%80%D1%8B%D0%B9%20%D0%B2%D0%B5%D1%87%D0%B5%D1%80%21%0A%D0%A5%D0%BE%D1%87%D1%83%20%D0%B7%D0%B0%D0%BF%D0%B8%D1%81%D0%B0%D1%82%D1%8C%20%D1%81%D0%B2%D0%BE%D1%8E%20%D0%BA%D0%BE%D0%BC%D0%B0%D0%BD%D0%B4%D1%83%20%D0%BD%D0%B0%20%D1%81%D0%BE%D1%80%D0%B5%D0%B2%D0%BD%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5%20EyeCode%20Battle%20%3A%20Summer%20edition%20!" className={styles.button}>
                 Зарегистрироваться через WhatsApp
               </a>
             </div>
